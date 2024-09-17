2510edb594bdb4648fabeadc83ce561d82b3db16
Sean10040 <sean.park@kingsway.school.nz>
2024-09-18 10:25:45 +1200

    Add files via upload

---
+++ Welcome.py
@@ -0,0 +1,12 @@
+# My first python program
+# Author: Sean Park
+# Date: 18/09/2024
+# Version: 1.0
+
+
+# This is a comment. It starts with a #
+# Print out a welcome message
+print("Hello World") 
+print("Welcome to 10DGT")
+print() #This creates a line break
+print('Hello\nWorld') #\n creates a line break
