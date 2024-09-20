94b612931a20002dab0397282b7c716991f8f150
Sean10040 <sean.park@kingsway.school.nz>
2024-09-20 14:34:09 +1200

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,65 @@
+# Showing how variables work
+# Author: Sean Park
+# Date: 20 September 2024
+# Version: 1
+
+
+# Different types of variables
+num_1 = 17
+print(num_1) #This displays what num_1 contains
+
+name= "Sean"
+print(name)
+
+string_eg1 = "This is a string"
+print(string_eg1)
+
+integer_eg = 10
+print(integer_eg)
+
+float_eg = 7.1414
+print(float_eg)
+
+bool_eg1 = "True"
+bool_eg2 = "False"
+print(bool_eg1)
+
+# If statements
+no1 = 10
+no2 = 10 
+
+if no1 == no2:
+    print("Yay")
+
+Cat = 6
+Dog = 8
+if Cat > Dog:
+    print("Cats are better than Dogs")
+elif Dog > Cat:
+    print("Dogs are better than Cats")
+else:
+    print("I don't like both cats and dogs")
+
+# Maths calculation
+sum = 1+2+3+4+5+6+7+8+9+10
+print(sum)
+
+#If statements
+Messi = 838 
+Cristiano_Ronaldo = 901
+
+if Messi > Cristiano_Ronaldo:
+    print("Messi scored more than Cristiano_Ronaldo")
+
+elif Cristiano_Ronaldo > Messi:
+    print("Cristiano_Ronaldo scored more than Messi")
+
+else:
+    print("They are both equally good")
+
+#Maths
+num_1 = 4
+num_2 = 9
+concatenation = num_1 + num_2 + 10
+print(concatenation)
+
