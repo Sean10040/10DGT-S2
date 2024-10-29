c5c857c7d0cc8732eb7c6e8cf15417a035dad6a9
Sean10040 <sean.park@kingsway.school.nz>
2024-10-30 09:36:25 +1300

    Area and Perimeter calculator

---
+++ Area and Perimeter.py
@@ -0,0 +1,19 @@
+#Error checking in python
+#Author: Sean Park
+#Date: 2024-10-30
+#Version 1
+
+
+#Ask the user for the width and height
+#Assume they put in valid data
+width = float(input("Width: "))
+height = float(input("Height: "))
+
+#Calculate Area and Perimeter 
+area = width * height
+perimeter = 2 * (width + height)
+
+#Output the area and perimeter
+print()
+print(f"Perimeter: {perimeter} units")
+print(f"Area: {area} square units")
