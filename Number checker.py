b45c8305a989eabd23c509e792fe9973e00491db
Sean10040 <sean.park@kingsway.school.nz>
2024-10-30 10:04:30 +1300

    Add files via upload

---
+++ Number checker.py
@@ -0,0 +1,20 @@
+#Number Checker
+#Author: Sean Park
+#Date: 2024-10-30
+#Version 1
+
+#Ask user for width and loop until they enter a number that is more than zero
+def num_check(question):...
+
+
+
+#Main routine goes here
+for item in range (0,2):
+    width = num_check ("width: ")
+    print(width)
+
+print()
+
+for item in range (0,2):
+    height = num_check ("Height: ")
+    print(height)
